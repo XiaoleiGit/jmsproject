fbc63e6f7489958f2ce2ab0015dd7ce2b1c431f8
Xiaolei <winny_wxl@hotmail.com>
2019-12-04 19:21:11 +0100

    first commit

---
+++ src/main/java/com/example/jmsproject/JmsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jmsproject/JmsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
